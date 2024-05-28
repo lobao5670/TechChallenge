2b41ffa14c0e300758e3f802fe8d20ae5ac04726
Gabriel Lobão <lobao5670@gmail.com>
2024-05-28 20:05:18 -0300

    Adicionar documentacao

--- app/api/endpoints/exportacao.py
+++ app/api/endpoints/exportacao.py
@@ -7,9 +7,27 @@ exportacao = Exportacao()
 
 @router.get("/quantidade")
 async def get_quantidade():
+    """
+    ### Retorna a quantidade de derivados de uva exportados.
+
+    Retorna um Dataframe formatado em JSON com:
+    - **classificacao**: classificacao da uva exportada
+    - **pais**: pais da uva exportada
+    - **ano**: ano referente à exportacao
+    - **quantidade**: quantidade em quilos exportado
+    """
     return exportacao.quantidade
 
 
 @router.get("/valor")
 async def get_valor():
+    """
+    ### Retorna a quantidade de derivados de uva exportados.
+
+    Retorna um Dataframe formatado em JSON com:
+    - **classificacao**: classificacao da uva exportada
+    - **pais**: pais da uva exportada
+    - **ano**: ano referente à exportacao
+    - **valor**: valor em dólares exportado
+    """
     return exportacao.valor
--- app/api/endpoints/importacao.py
+++ app/api/endpoints/importacao.py
@@ -7,9 +7,27 @@ importacao = Importacao()
 
 @router.get("/quantidade")
 async def get_quantidade():
+    """
+    ### Retorna a quantidade de derivados de uva importados.
+
+    Retorna um Dataframe formatado em JSON com:
+    - **classificacao**: classificacao da uva importada
+    - **pais**: pais da uva importada
+    - **ano**: ano referente à importacao
+    - **quantidade**: quantidade em quilos importado
+    """
     return importacao.quantidade
 
 
 @router.get("/valor")
 async def get_valor():
+    """
+    ### Retorna a quantidade de derivados de uva importados.
+
+    Retorna um Dataframe formatado em JSON com:
+    - **classificacao**: classificacao da uva importada
+    - **pais**: pais da uva importada
+    - **ano**: ano referente à importacao
+    - **valor**: valor em dólares importado
+    """
     return importacao.valor
